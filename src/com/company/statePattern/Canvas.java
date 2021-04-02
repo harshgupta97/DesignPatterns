fae716acaadb553738eb32e4322a508273613199
Harsh Gupta <hgupta7991@gmail.com>
2021-04-02 11:06:49 +0530

    added state pattern defination in the canvas class

--- src/com/company/statePattern/Canvas.java
+++ src/com/company/statePattern/Canvas.java
@@ -1,5 +1,8 @@
 package com.company.statePattern;
 
+// In state pattern a class behavior changes based on its state. This type of design pattern comes under behavior pattern
+
+// In state pattern, we create objects which represent various states and a context object whose behaviour varies as its state  object changes.
 public class Canvas {
     private Tool currentTool;
 
